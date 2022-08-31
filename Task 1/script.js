a2c6450d7f40559d14072050ba54a425dd96c399
Kestutis Voras <kestas.voras@gmail.com>
2022-08-31 19:34:00 +0300

    Pirma užduotis done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,22 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const kg = search.value * 1;
+  console.log(Number.isInteger(kg));
+
+  if (Number.isInteger(kg)) {
+    let lb = Math.round(kg * 2.2046 * 100) / 100;
+    let g = Math.round(kg * 0.001 * 100) / 100;
+    let oz = Math.round(kg * 35.274 * 100) / 100;
+    output.innerHTML = `<span>Svarai: ${lb}</span><span>Gramai: ${g}</span><span>Uncijos: ${oz}</span>`;
+  } else {
+    alert("Kilogramai privalo būti išreikšti skaičiumi");
+    search.value = "";
+    output.innerHTML = "";
+  }
+});
