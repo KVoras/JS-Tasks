7b98518b81d40938bad249d8a163118573eb2ff0
Kestutis Voras <kestas.voras@gmail.com>
2022-09-01 19:44:04 +0300

    Trečia užduotis done

--- Task 3/script.js
+++ Task 3/script.js
@@ -13,37 +13,40 @@ turėti bent minimalų stilių ir būti responsive;
 
 const ENDPOINT = "https://api.github.com/users";
 
-let users;
+document.getElementById("btn").addEventListener("click", printUsers);
 
-fetch(ENDPOINT)
-  .then((res) => {
-    console.log(res);
-    return res.json();
-  })
-  .then((users) => {
-    console.log(users);
-    users = users;
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+function printUsers() {
+  let output = document.getElementById("output");
+  output.style.display = "grid";
+  output.style.gridTemplateColumns = "25% 25% 25% 25%";
+  fetch(ENDPOINT)
+    .then((res) => res.json())
 
-function showUsers(users) {
-  const output = document.getElementById("output");
-  output.innerHTML = "";
+    .then((json) => {
+      json.forEach((user) => {
+        let card = document.createElement("div");
+        card.style.textAlign = "center";
+        card.style.padding = "3px";
+        card.style.margin = "2px";
+        card.style.border = "solid grey";
+        card.style.borderRadius = "5px";
 
-  users.forEach((user) => {
-    const login = document.createElement("H3");
-    login.innerText = user.login;
+        let userAvatar = document.createElement("img");
+        userAvatar.src = user.avatar_url;
+        userAvatar.alt = "User Avatar";
+        userAvatar.style.width = "100%";
 
-    const avatar = document.createElement("img");
-    avatar.src = user.avatar_url;
-    avatar.setAttribute("alt", "UserAvatar");
+        let userName = document.createElement("h4");
+        userName.innerText += user.login;
 
-    const container = document.createElement("div");
-    container.append(login, avatar);
-    output.append(container);
-  });
+        card.appendChild(userAvatar);
+
+        card.appendChild(userName);
+
+        output.appendChild(card);
+
+        const message = document.getElementById("message");
+        message.style.display = "none";
+      });
+    });
 }
-
-showUsers(users);
